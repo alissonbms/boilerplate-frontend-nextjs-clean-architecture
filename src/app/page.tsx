f748769d53f5f0134abb67e32b32527d7c01e86c
alissonbms <alissonnewdev@gmail.com>
2025-02-14 17:28:35 -0300

    feat: create feature to add users to a list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
+"use client";
+
+import { FormEvent, useState } from "react";
 import styles from "./styles/page.module.scss";
+import { useAppStore } from "./store";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const { users, setUsers } = useAppStore();
+
+  function handleAddUser(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (/^\s/.test(input)) {
+      alert("Empty field");
+    } else {
+      const newUser = {
+        id: Math.floor(Math.random() * 10000),
+        name: input,
+      };
+      setUsers([...users, newUser]);
+    }
+  }
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "5px",
+        padding: "10px",
+      }}
+    >
       <h1>Hello world</h1>
+      <form onSubmit={(e) => handleAddUser(e)}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          required={true}
+        />
+        <button type="submit">Add user</button>
+      </form>
+      <ul style={{ paddingLeft: "20px" }}>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
